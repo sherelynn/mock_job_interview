d0fdc66ba069ded5c8c853a16e8fda52df254ff3
sherelynn <sherelynnb@missionreadyhq.com>
2025-04-16 09:15:45 +1200

    initial commit
    - import configured app instance and port from config
    - startup server

--- mock_interview_backend/server.js
+++ mock_interview_backend/server.js
@@ -1,2 +1,11 @@
 //========== Main Entry Point ==========//
 //========== Startup Server ==========//
+
+const app = require("./src/app") // Import the configured Express app
+const config = require("./src/config") // Import PORT from config
+
+const PORT = config.port
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhostL:${PORT}`)
+})
